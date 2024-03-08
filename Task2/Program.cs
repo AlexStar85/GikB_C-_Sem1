9ca9b364242bd6b7d0ef886de378bc987a39e884
Alex Star <N690@yandex.ru>
2024-03-09 01:03:49 +0300

    Отладка Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,7 +8,7 @@
 Console.WriteLine("Введите целое положительное число n: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int count = -n;
-while (count <= n) ;
+while (count <= n)
 {
     Console.Write(count + " ");
     count = count + 1;
