05878a28f6240234b98cd5e192638ff71a312faa
Alex Star <N690@yandex.ru>
2024-03-09 01:42:30 +0300

    add Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,3 +4,16 @@
 // 782 => 9
 // 918 => 17
 
+Console.WriteLine("Введите целое трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num <= 999 || num >= -999 && num <= -100)
+{
+    int firstDigt = num / 100;
+    int lastDigt = num % 10;
+    int sum = firstDigt + lastDigt;
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
